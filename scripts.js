a7c8ec9af99d0d08e652816e01f1d32eb6167e52
Camila Tuñas <camila_tunas@hotmail.com>
2021-05-16 20:07:39 -0300

    intentos

--- scripts.js
+++ scripts.js
@@ -1,7 +1,34 @@
 const prueba = document.getElementById('modoClaro');
+//const nombreBoton = document.getElementsByClassName('nombre-boton');
 
 const toggleMode = () => {
-    document.body.classList.toggle("dark")
+    document.body.classList.toggle("dark");
+    // if (document.body.classList == 'dark') {
+    //     nombreBoton.innerText = 'Modo claro';
+    // }else {
+    //     nombreBoton.innerText = 'Modo oscuro';
+    // }
 }
-
 prueba.addEventListener('click', toggleMode); 
+
+// document.body.classList = 'modoClaro'
+// document.prueba = prueba.innerText('modo oscuro')
+// prueba.innerText = 'modo oscuro'
+
+// const cambiarModoOscuro = () => {
+//     document.body.classList.remove('light-theme')
+//     document.body.classList.add('dark-theme')
+//   }
+//   const cambiarModoClaro = () => {
+//     document.body.classList.remove('dark-theme')
+//     document.body.classList.add('light-theme')
+//   }
+
+const imagenURL = document.getElementById('url');
+const imagenMeme = document.getElementById('imagenID');
+
+function imagenURLMeme() {
+    let url = imagenURL.value;
+    imagenMeme.style.backgroundImage = 'url';
+}
+imagenURL.addEventListener('change', imagenURLMeme);
